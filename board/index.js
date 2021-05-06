41d857f48d48f2004792d06e0e5c448deb05d349
박인성 <shfoemesmsrPwjd1@gmail.com>
2021-05-07 08:20:26 +0900

    db setting

--- board/index.js
+++ board/index.js
@@ -6,16 +6,20 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var app = express();
 
-// DB setting
+// DB setting : mongoose의 몇몇 글로벌 설정을 해 주는 부분
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGO_DB);
-var db = mongoose.connection;
+mongoose.connect(process.env.MONGO_DB); // process.env : db에 연결할수 있다.
+var db = mongoose.connection; // mongoose의 db object를 가져와 db변수에 넣는 과정
+
+// db가 성공적으로 연결된 경우 "DB connected"를 출력
 db.once('open', function(){
   console.log('DB connected');
 });
+
+// db연결중 에러가 있는 경우 "DB ERROR : " 와 에러를 출력
 db.on('error', function(err){
   console.log('DB ERROR : ', err);
 });
